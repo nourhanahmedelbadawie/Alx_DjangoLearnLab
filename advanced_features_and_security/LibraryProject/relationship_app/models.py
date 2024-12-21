a1a5a9c72840020387c96b22010ae027e7aa08d2
Nourhan Ahmed <46425742+nourhanahmedelbadawie@users.noreply.github.com>
2024-12-21 18:15:00 +0200

    0.3

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save  # Import the signal for post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.conf import settings
+
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -74,49 +73,3 @@ class Book(models.Model):
             ("can_change_book", "Can change book"),
             ("can_delete_book", "Can delete book"),
         ]
-
-
-
-
-
-class CustomUserManager(BaseUserManager):
-    """
-    Custom manager for CustomUser with `date_of_birth` and `profile_photo`.
-    """
-
-    def create_user(self, email, username, password=None, **extra_fields):
-        if not email:
-            raise ValueError("The Email field is required")
-        email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, username, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if not extra_fields.get("is_staff"):
-            raise ValueError("Superuser must have is_staff=True.")
-        if not extra_fields.get("is_superuser"):
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self.create_user(email, username, password, **extra_fields)
-
-
-class CustomUser(AbstractUser):
-    """
-    Custom user model extending Django's AbstractUser.
-    """
-    email = models.EmailField(unique=True)
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.username
-
-class ExampleModel(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- advanced_features_and_security/LibraryProject/relationship_app/settings.py
+++ advanced_features_and_security/LibraryProject/relationship_app/settings.py
@@ -16,4 +16,3 @@ TEMPLATES = [
         },
     },
 ]
-AUTH_USER_MODEL = 'users.CustomUser'
