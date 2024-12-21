72d8c829ae17cacedb6d68324b2d45bb517638c8
Nourhan Ahmed <46425742+nourhanahmedelbadawie@users.noreply.github.com>
2024-12-21 15:55:40 +0200

    4.4

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import  list_books , LibraryDetailView# This is the missing import
 from django.contrib.auth import views as views
-from .views import admin_view, librarian_view, member_view , edit_book , delete_book
+from .views import admin_view, librarian_view, member_view , edit_book , delete_book , add_book
 from . import views
 
 urlpatterns = [
@@ -12,9 +12,9 @@ urlpatterns = [
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:book_id>/edit/', edit_book, name='edit_book'),
-    path('books/<int:book_id>/delete/', delete_book, name='delete_book'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/<int:book_id>/edit_book/', edit_book, name='edit_book'),
+    path('books/<int:book_id>/delete_book/', delete_book, name='delete_book'),
     path('books/', list_books, name='list_books'),
 
 ]
