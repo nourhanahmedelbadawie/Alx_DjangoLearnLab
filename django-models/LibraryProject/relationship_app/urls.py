eb873832028d02c4e78b0f8896882085bf971a1a
Nourhan Ahmed <46425742+nourhanahmedelbadawie@users.noreply.github.com>
2024-12-21 14:41:20 +0200

    2.2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
-from .views import register  # Import the register view from views.py
+from .views import register , list_books , LibraryDetailView# This is the missing import
 from django.contrib.auth import views as auth_views
 
 
@@ -9,5 +8,5 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),  # Add the register path and link it to the register view
+    path('register/', register, name='register'),  # Register the path for registration
 ]
